576e3e10dcbbe33ea27514838c599201e97507d6
nlom0218 <nlom0218@naver.com>
2021-06-03 23:19:55 +0900

    introspection true

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   playground: true,
+  introspection: true,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token)
