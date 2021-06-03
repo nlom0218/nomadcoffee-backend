9fc30b41e76879ae38ef531a6af6beb4c7a3ced2
nlom0218 <nlom0218@naver.com>
2021-06-03 22:56:31 +0900

    first heroku commit

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ nomadcoffee Backend.
 - [x] User Model
 - [x] User Profile
 - [x] Follow Me!
-- [ ] Models & Resolvers (CoffeShopPhoto, CoffeeShop, Category)
+- [x] Models & Resolvers (CoffeShopPhoto, CoffeeShop, Category)
---
+++ coffeeShops/uploadCoffeeShopPhoto copy/uploadCoffeeShopPhoto.resolvers.js
@@ -0,0 +1,41 @@
+import client from "../../src/client";
+import { uploadPhoto } from "../../shared/shared";
+import { protectedResolver } from "../../src/users/users.utils";
+
+export default {
+  Mutation: {
+    uploadCoffeeShopPhoto: protectedResolver(
+      async (_, { coffeeShopId, photo }, { loggedInUser }) => {
+        const coffeeShop = await client.coffeeShop.findUnique({
+          where: { id: coffeeShopId },
+          select: { userId: true }
+        })
+        if (!coffeeShop) {
+          return {
+            ok: false,
+            error: "해당 커피숍을 찾을 수 없습니다."
+          }
+        } else if (coffeeShop.userId !== loggedInUser.id) {
+          return {
+            ok: false,
+            error: "해당 커피숍 사진 업로드 권한이 없습니다."
+          }
+        }
+        const coffeeShopPhotoURL = await uploadPhoto(photo, loggedInUser)
+
+        await client.coffeeShop.update({
+          where: { id: coffeeShopId },
+          data: {
+            photos: {
+              create: { url: coffeeShopPhotoURL }
+            }
+          }
+        })
+
+        return {
+          ok: true
+        }
+      }
+    )
+  }
+}
