ab04468aee658ba026fe5137d0e63e8eebb1ee51
nlom0218 <nlom0218@naver.com>
2021-07-03 18:05:57 +0900

    add category

--- src/coffeeShops/editCategory/editCategory.resolvers.js
+++ src/coffeeShops/editCategory/editCategory.resolvers.js
@@ -1,6 +1,6 @@
 import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
-import { removeCategories } from "../coffeeShops.utils";
+import { processCategories, removeCategories } from "../coffeeShops.utils";
 
 export default {
   Mutation: {
@@ -26,6 +26,28 @@ export default {
         data: {
           categories: {
             disconnect: removeCategories(categories)
+          }
+        }
+      })
+      return {
+        ok: true
+      }
+    }),
+    addCategory: protectedResolver(async (_, { categories, shopId }, { loggedInUser }) => {
+      const user = await client.user.findFirst({
+        where: { shop: { some: { id: shopId } } }
+      })
+      if (user.id !== loggedInUser.id) {
+        return {
+          ok: false,
+          error: "삭제 권한이 없습니다."
+        }
+      }
+      await client.coffeeShop.update({
+        where: { id: shopId },
+        data: {
+          categories: {
+            connectOrCreate: processCategories(categories)
           }
         }
       })
